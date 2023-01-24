865105435c284328a4be52945a500d1899706cee
delso-ferreira <delsofelipe@gmail.com>
2023-01-24 20:56:45 -0300

    começa

--- exercicios-do-dia/scripts.js
+++ exercicios-do-dia/scripts.js
@@ -16,7 +16,7 @@ for ( let index = 0; index < numbers.length ; index += 1) {
     }
     console.log(sum); */
 
-// calcular média aritmética
+// calcular média aritmética!
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
