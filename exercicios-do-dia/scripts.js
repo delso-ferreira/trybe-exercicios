e797b1123b2eafaf65e445c67e092b8803b3d57b
delso-ferreira <delsofelipe@gmail.com>
2023-01-24 21:18:36 -0300

    media e valor 20

--- exercicios-do-dia/scripts.js
+++ exercicios-do-dia/scripts.js
@@ -16,13 +16,24 @@ for ( let index = 0; index < numbers.length ; index += 1) {
     }
     console.log(sum); */
 
-// calcular média aritmética!
+// calcular média aritmética e console com valor maior ou menor que 20
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 let average = 0;
 
 for ( let index = 0; index < numbers.length ; index += 1) { 
-    soma += numbers[index] , average = soma / numbers.length
+    soma += numbers[index];
     }
-    console.log(average);
+    
+    average = soma / numbers.length
+
+    if (average > 20); {
+        console.log('Valor maior que 20');    
+    }
+    else (average <= 20); {
+        console.log('Valor menor ou igual a 20');
+    }
+    
+ 
+    
