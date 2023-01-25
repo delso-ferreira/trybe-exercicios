ad8257322689ca6e8d46c95d8c2e1541bea16bc1
delso-ferreira <delsofelipe@gmail.com>
2023-01-24 22:04:35 -0300

    continua exercício

--- exercicios-do-dia/scripts.js
+++ exercicios-do-dia/scripts.js
@@ -18,7 +18,7 @@ for ( let index = 0; index < numbers.length ; index += 1) {
 
 // calcular média aritmética e console com valor maior ou menor que 20
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 let average = 0;
 
@@ -33,7 +33,32 @@ for ( let index = 0; index < numbers.length ; index += 1) {
     }
     else (average <= 20); {
         console.log('Valor menor ou igual a 20');
+    } */
+
+    // Achar maior número no array
+    
+  /*   let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    
+    let maiorNumero = numbers[0];
+
+    for (index = 1; index < numbers.length; index += 1){
+        if (numbers[index] > maiorNumero) {
+            maiorNumero = numbers[index]
+        }
     }
+    console.log(maiorNumero); */
     
- 
-    
+    //  Valores ímpares dentro do array
+
+    /* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+        
+    for (index = 0; index < numbers.length; index += 1){
+        if (numbers[index] % 2 ) {
+            console.log(numbers[index])
+            
+        }
+        else {
+            console.log('Nenhum valor ímpar encontrado');
+        }
+        }
+ */
